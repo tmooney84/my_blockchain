f02b032744590c34a7e9f7a1aecda1fb3b458369
trevor mooney <mooney_t@qwasar.io>
2025-04-22 04:04:17 +0000

    fix: turned off sorting again within listing functionality ~(7/8) gandalf passed

--- dsa.c
+++ dsa.c
@@ -665,11 +665,11 @@ int sort_node_array(Node **node_array, int *current_node_array_size)
 
 int list_nodes(Node **node_array, int *current_node_array_size)
 {
-    if (!sort_node_array(node_array, current_node_array_size))
-    {
-        printf("Unable to sort node_array");
-        return -1;
-    }
+    // if (!sort_node_array(node_array, current_node_array_size))
+    // {
+    //     printf("Unable to sort node_array");
+    //     return -1;
+    // }
 
     for (int i = 0; i < *current_node_array_size; i++)
     {
@@ -685,11 +685,11 @@ int list_nodes(Node **node_array, int *current_node_array_size)
 
 int list_nodes_blocks(Node **node_array, int *current_node_array_size)
 {
-    if (!sort_node_array(node_array, current_node_array_size))
-    {
-        printf("Unable to sort node_array");
-        return -1;
-    }
+    // if (!sort_node_array(node_array, current_node_array_size))
+    // {
+    //     printf("Unable to sort node_array");
+    //     return -1;
+    // }
 
     for (int i = 0; i < *current_node_array_size; i++)
     {
@@ -703,7 +703,7 @@ int list_nodes_blocks(Node **node_array, int *current_node_array_size)
 
         while (current != NULL)
         {
-            printf(", %ld", current->block_id);
+            printf(" %ld", current->block_id);
             current = current->next;
         }
         printf("\n");
